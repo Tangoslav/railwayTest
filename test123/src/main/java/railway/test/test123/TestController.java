6d6c5837cbadb09a7dcffe8d0b02c184eccecde6
Tobias Le <letobias@fel.cvut.cz>
2024-04-17 19:39:06 +0200

    fixed test controller

--- test123/src/main/java/railway/test/test123/TestController.java
+++ test123/src/main/java/railway/test/test123/TestController.java
@@ -1,9 +1,11 @@
 package railway.test.test123;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("/test")
 public class TestController {
     @GetMapping
     public String test() {
